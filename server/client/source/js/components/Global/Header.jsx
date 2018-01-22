54310d7b4a37bd346464adbb61386333f24eab62
Dastan <dastan.zhanibek@gmail.com>
2018-01-22 17:52:35 +0800

    navbar collapsible

--- server/client/source/js/components/Global/Header.jsx
+++ server/client/source/js/components/Global/Header.jsx
@@ -1,44 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { routeCodes } from '../../views/App';
+const classNames = require('classnames');
 
-const Header = () => {
-  return (
-    <nav className='navbar is-black is-transparents header '>
-      <div className='navbar-brand nav-logo'>
-        <NavLink className='navbar-item' activeClassName='' to={ routeCodes.HOME }>
-          Bard From The East
-        </NavLink>
+class Header extends Component {
+  state = {
+    burgerIsActive: false
+  };
 
-        <div className='navbar-burger burger' data-target='navbarExampleTransparentExample'>
-          <span />
-          <span />
-          <span />
+  render() {
+    let navbarBurgerClassName = classNames({
+      "navbar-burger burger": true,
+      'is-active': this.state.burgerIsActive
+    });
+    let navbarMenuClassName = classNames({
+      'navbar-menu navbar-end app-menu': true,
+      'is-active': this.state.burgerIsActive
+    });
+    return (
+      <nav className="navbar is-black is-transparents header ">
+        <div className="navbar-brand nav-logo">
+          <NavLink
+            className="navbar-item"
+            activeClassName=""
+            to={routeCodes.HOME}
+          >
+            Bard From The East
+          </NavLink>
+
+          <div
+            className={navbarBurgerClassName}
+            onClick={() =>
+              this.setState({ burgerIsActive: !this.state.burgerIsActive })
+            }
+          >
+            <span />
+            <span />
+            <span />
+          </div>
         </div>
-      </div>
 
-      <div className='navbar-menu navbar-end app-menu'>
-        <NavLink className='navbar-item' activeClassName='active' to={ routeCodes.WHATWEDO }>
-          WhatWeDo
-        </NavLink>
-        <NavLink className='navbar-item' activeClassName='active' to={ routeCodes.MEDIA }>
-          Media
-        </NavLink>
-        <NavLink className='navbar-item' activeClassName='active' to={ routeCodes.COURSE }>
-          Course
-        </NavLink>
-        <NavLink className='navbar-item' activeClassName='active' to={ routeCodes.RENTAL }>
-          Rental
-        </NavLink>
-        <NavLink className='navbar-item' activeClassName='active' to={ routeCodes.ABOUT }>
-          Team
-        </NavLink>
-        <NavLink className='navbar-item' activeClassName='active' to={ routeCodes.CONTACT }>
-          Contact
-        </NavLink>
-      </div>
-    </nav>
-  );
-};
+        <div className={navbarMenuClassName}>
+          <NavLink
+            className="navbar-item"
+            activeClassName="active"
+            to={routeCodes.WHATWEDO}
+          >
+            WhatWeDo
+          </NavLink>
+          <NavLink
+            className="navbar-item"
+            activeClassName="active"
+            to={routeCodes.MEDIA}
+          >
+            Media
+          </NavLink>
+          <NavLink
+            className="navbar-item"
+            activeClassName="active"
+            to={routeCodes.COURSE}
+          >
+            Course
+          </NavLink>
+          <NavLink
+            className="navbar-item"
+            activeClassName="active"
+            to={routeCodes.RENTAL}
+          >
+            Rental
+          </NavLink>
+          <NavLink
+            className="navbar-item"
+            activeClassName="active"
+            to={routeCodes.ABOUT}
+          >
+            Team
+          </NavLink>
+          <NavLink
+            className="navbar-item"
+            activeClassName="active"
+            to={routeCodes.CONTACT}
+          >
+            Contact
+          </NavLink>
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Header;
